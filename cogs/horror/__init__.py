03cd1f6462774551cd4ca68274c881ada69a768c
beycord <princekhan958282@gmail.com>
2026-09-14 23:03:27 +0530

    Load Horror settings authorization fix

--- cogs/horror/__init__.py
+++ cogs/horror/__init__.py
@@ -2,11 +2,13 @@
 
 from .horror import setup as _setup_horror
 from .unknown_battle import setup as _setup_unknown
+from .settings_fix import setup as _setup_settings_fix
 
 
 async def setup(bot) -> None:
     await _setup_horror(bot)
     await _setup_unknown(bot)
+    await _setup_settings_fix(bot)
 
 
 __all__ = ["setup"]
